d804bbb90c58b117570789a3d591f9ff818eb9f4
JSk00g <joel.skoog@student.forsbergsskola.se>
2024-10-07 14:57:19 +0200

    Finnished p10

---
+++ Projects11/P10Booleans/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("What is your age");
+
+int age = int.Parse (Console.ReadLine ());
+
+bool isChild = age > 0 && age < 12;
+bool isTeen = age > 13 && age < 19;
+bool isAdult = age > 19 && age < 100; 
+
+Console.WriteLine ($"You are a Child: {isChild}");
+Console.WriteLine ($"You are a Teen: {isTeen}");
+Console.WriteLine ($"You are an Adult: {isAdult}");
